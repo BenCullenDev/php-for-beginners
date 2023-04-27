8f5332b9eb6bc03d0ddc78c11691735f771819f9
Ben Cullen <benjaminjcullen1@gmail.com>
2023-04-27 09:02:56 +0100

    Lesson 26 - Persist note body in DB, htmlspecialchars to avoid styling and alerts.

--- views/note.view.php
+++ views/note.view.php
@@ -5,7 +5,7 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h2 class="text-2xl font-bold"><?= $note['body'] ?></h2>
+                    <h2 class="text-2xl font-bold"><?= htmlspecialchars($note['body']) ?></h2>
                 </div>
             </div>
             <a href="/notes">
